959185ac534c0af47567e9225c0cc8da31431fee
Dev-Master-Flex <102252995+Dev-Master-Flex@users.noreply.github.com>
2023-01-18 13:31:09 -0600

    Update InuV2_Das.sol

--- InuV2_Das/InuV2_Das.sol
+++ InuV2_Das/InuV2_Das.sol
@@ -199,8 +199,8 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
 }
 contract Inuvation is ERC20, Ownable {
     using SafeMath for uint256;
-    IUniswapV2Router02 public immutable uniswapV2Router;
-    address public immutable uniswapV2Pair;
+    IUniswapV2Router02 public uniswapV2Router;
+    address public uniswapV2Pair;
     uint8 private constant _decimals = 9;
     address public constant deadAddress = address(0xdead);
     address private utility1Address;
